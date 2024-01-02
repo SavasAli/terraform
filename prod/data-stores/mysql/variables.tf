60e4ed128a3076a64c018465be0b3ed3f40bd385
SavasAli <ozdemir.savasali@gmail.com>
2024-01-02 21:21:41 +0100

    Finish module example

--- prod/data-stores/mysql/variables.tf
+++ prod/data-stores/mysql/variables.tf
@@ -3,16 +3,16 @@
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket used for the database's remote state storage"
-  type        = string
-  default     = "terraform-up-and-running-state-savas"
+variable "db_username" {
+    description = "The username for the database"
+    type        = string
+    sensitive   = true
 }
 
-variable "db_remote_state_key" {
-  description = "The name of the key in the S3 bucket used for the database's remote state storage"
-  type        = string
-  default     = "stage/services/data-stores/mysql/terraform.tfstate"
+variable "db_password" {
+    description = "The password for the database"
+    type        = string
+    sensitive   = true
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -20,8 +20,8 @@ variable "db_remote_state_key" {
 # These parameters have reasonable defaults.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "cluster_name" {
-  description = "The name to use to namespace all the resources in the cluster"
-  type        = string
-  default     = "webservers-stage"
+variable "db_name" {
+    description = "The name to use for the database"
+    type        = string
+    default     = "example_database"
 }
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
   }
 
-  backend "s3" {
+    backend "s3" {
     bucket  = "terraform-up-and-running-state-savas"
     key     = "stage/services/webserver-cluster/terraform.tfstate"
     region  = "us-east-2"    
@@ -22,155 +22,14 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0fb653ca2d3203ac1" # Amazon Machine Image 
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id] # Use security group "Instance" defined below (implicit dependency)
+module "webserver-cluster" {
+  source = "../../../modules/services/webserver-cluster"
 
-  # Run a web server that always returns the text "Hello world" 
-  user_data = templatefile("user-data.sh", {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-  })
+  cluster_name            = var.cluster_name
+  db_remote_state_bucket  = var.db_remote_state_bucket
+  db_remote_state_key     = var.db_remote_state_key
 
-  # Required when using a launch configuration with an auto scaling group.
-  # Launch configuration is set through a reference. 
-  # Terraform won't be able to change the launch configuration, since this asg depends on it through this reference.
-  # Therefore the new resource will have to be created first, before the old can be deleted.
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-# Security group allows all incoming requests on 80 and allows all outgoing requests so the load balancer can perform health checks.
-resource "aws_security_group" "instance" {
-  name = var.instance_security_group_name
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "example" {
-  name               = var.alb_name
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-# This target group with health check the EC2 instances
-# How does the target group know which EC2 instances to target?
-# A static list won't work, since EC2 instances can launch and terminate any moment
-# Take advantage of the first-class integration between the ASG and LB
-# Set the target_group_arns of the aws_autoscaling group to point at the new target group
-resource "aws_lb_target_group" "asg" {
-  name     = var.alb_name
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-resource "aws_security_group" "alb" {
-  name = var.alb_security_group_name
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "us-east-2"
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
 }
